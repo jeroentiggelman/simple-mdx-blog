4765d712694c149c58425a51c684e0d42436e433
jeroentiggelman <infos@photographicdesign.be>
2020-06-17 13:05:29 +0200

    Added content component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          date="March 22, 2020"
+          title="Lorem ipsum dolor sit amet"
+          excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices felis mattis tincidunt dictum. Mauris."
+          slug="/test"
+        />
+      </Content>
     </Container>
   )
 }
